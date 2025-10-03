1827af5ccfaa9bb1a1c9552df1ccff86b590d79a
reloaddev <henrik.pruess@posteo.de>
2025-10-03 13:09:05 +0200

    Adapt unit test to check for AST object instead of AST string

--- src/proton_pack/migration/parser.py
+++ src/proton_pack/migration/parser.py
@@ -1,4 +1,4 @@
 from sqlglot import parse_one
 
 def parse_sql_to_ast(sql):
-    return repr(parse_one(sql))
+    return parse_one(sql, dialect="postgres")
--- test/tests/migration/test_parser.py
+++ test/tests/migration/test_parser.py
@@ -1,13 +1,15 @@
-import pytest
 from proton_pack.migration.parser import parse_sql_to_ast
+from sqlglot import exp
 
 
 def test_parse_sql_to_ast():
-    assert parse_sql_to_ast("SELECT * FROM ghosts").strip() == """
-Select(
-  expressions=[
-    Star()],
-  from=From(
-    this=Table(
-      this=Identifier(this=ghosts, quoted=False))))
-   """.strip()
+    # given
+    sql_string = "SELECT * FROM ghosts;"
+
+    # when
+    ast = parse_sql_to_ast(sql_string)
+
+    # then
+    assert ast.find(exp.Select) is not None
+    assert ast.expressions[0].sql() == "*"
+    assert ast.find(exp.Table).this.sql() == "ghosts"
