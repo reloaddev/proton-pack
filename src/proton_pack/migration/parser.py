44900154a31d8d43701350ebfe2e9e468d77e223
reloaddev <henrik.pruess@posteo.de>
2025-10-09 14:37:37 +0200

    Filter out None values

--- src/proton_pack/migration/parser.py
+++ src/proton_pack/migration/parser.py
@@ -4,4 +4,5 @@ from sqlglot import exp, parse
 
 
 def parse_sql_to_ast(sql) -> List[exp.Expression]:
-    return parse(sql, dialect="postgres")
+    parsed = parse(sql, dialect="postgres")
+    return [p for p in parsed if p is not None]
