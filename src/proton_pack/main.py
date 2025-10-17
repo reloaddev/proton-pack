35aabbb1b3ed68a5600badc30e7e0c9a386f3438
reloaddev <henrik.pruess@posteo.de>
2025-10-17 20:06:33 +0200

    Remove CLI commands to parse and analyze non-file input

--- src/proton_pack/main.py
+++ src/proton_pack/main.py
@@ -9,17 +9,9 @@ def cli():
     pass
 
 
-@cli.command(name="debug")
-@click.argument("sql")
-def debug(sql):
-    click.echo(f"\n\nSQL \n===\n {sql} \n\n")
-    ast = parse_sql_to_ast(sql)
-    click.echo(f"AST \n===\n {ast} \n\n")
-
-
-@cli.command(name="debug-file")
+@cli.command(name="parse")
 @click.argument("file_path")
-def debug_file(file_path):
+def parse(file_path):
     with open(file_path, "r") as f:
         sql = f.read()
     click.echo(f"\n\nSQL \n===\n {sql} \n\n")
@@ -28,27 +20,8 @@ def debug_file(file_path):
 
 
 @cli.command(name="analyze")
-@click.argument("sql")
-def analyze_migration(sql):
-    ast = parse_sql_to_ast(sql)
-    result = analyze_ast(ast)
-    if result["DROP_DETECTED"]:
-        click.echo("Migration check failed! DROP statements detected.")
-        sys.exit(1)
-    if result["FOREIGN_KEY_WITHOUT_SUPP_INDEX"]:
-        click.echo("Migration check failed! Foreign key without supplementary index detected.")
-        sys.exit(1)
-    if result["NON_CONCURRENT_INDEX_BUILDS"]:
-        click.echo("Migration check failed! Found index, that is not concurrently built.")
-        sys.exit(1)
-
-    click.echo("Migration check passed.")
-    sys.exit(0)
-
-
-@cli.command(name="analyze-file")
 @click.argument("file_path")
-def analyze_migration_file(file_path):
+def analyze(file_path):
     with open(file_path, "r") as f:
         sql = f.read()
     ast = parse_sql_to_ast(sql)
