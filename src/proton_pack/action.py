4326d8b046b2e95eb53157aa6f9b97daa4a3f4f0
reloaddev <henrik.pruess@posteo.de>
2025-10-19 14:15:50 +0200

    Move printing of result into action (this is required, because right now the action fails for faulty migrations, meaning that the pipeline would abort, before printing the result, if we were to stick with the old approach)

--- src/proton_pack/action.py
+++ src/proton_pack/action.py
@@ -2,6 +2,7 @@ import sys
 
 from proton_pack.migration.parser import parse_sql_to_ast
 from proton_pack.migration.analyzer import analyze_ast
+from proton_pack.pretty_printer import pretty_print
 
 import os
 
@@ -11,6 +12,9 @@ with open(file_path, "r") as f:
 ast = parse_sql_to_ast(sql)
 result = analyze_ast(ast)
 
+pretty_print(result)
+
+# Write to output for further processing, currently unused
 output_path = os.environ.get('GITHUB_OUTPUT')
 with open(output_path, 'a') as f:
     f.write(f'analysis_result={result}\n')
