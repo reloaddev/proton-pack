4eec5f797349438a3179a0a2efa2027d147fcac9
reloaddev <henrik.pruess@posteo.de>
2025-11-07 15:26:09 +0100

    Allow parsing directories (of migration .sql files)

--- src/proton_pack/main.py
+++ src/proton_pack/main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import click
 
@@ -12,10 +13,17 @@ def cli():
 
 
 @cli.command(name="parse")
-@click.argument("file_path")
-def parse(file_path):
-    with open(file_path, "r") as f:
-        sql = f.read()
+@click.argument("path")
+def parse(path):
+    if os.path.isfile(path):
+        with open(path, "r") as f:
+            sql = f.read()
+    elif os.path.isdir(path):
+        sql = _read_migration_directory(path)
+    else:
+        click.echo("No migration files found.")
+
+
     click.echo(f"\n\nSQL \n===\n {sql} \n\n")
     ast = parse_sql_to_ast(sql)
     click.echo(f"AST \n===\n {ast} \n\n")
@@ -35,3 +43,23 @@ def analyze(file_path):
 
     click.echo("Migration check passed.")
     sys.exit(0)
+
+
+def _read_migration_directory(directory_path) -> str | None:
+    directory = os.fsencode(directory_path)
+    if directory is None:
+        return None
+
+    read_content = ""
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        filename_path = f"{directory_path}/{filename}"
+        if os.path.isdir(filename_path):
+            continue
+
+        if filename.endswith(".sql"):
+            with open(filename_path, "r") as opened_file:
+                read_content += "\n\n"
+                read_content += opened_file.read()
+
+    return read_content
