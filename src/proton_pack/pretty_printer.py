0f1a7baecdb1542d73a5194fdee8f2621e0b5098
reloaddev <henrik.pruess@posteo.de>
2025-11-12 18:56:41 +0100

    Add unit test for pretty_printer

--- src/proton_pack/pretty_printer.py
+++ src/proton_pack/pretty_printer.py
@@ -7,24 +7,24 @@ from .migration.parser import parse_sql_line_to_ast
 
 def pretty_print(sql, result):
     if not any(result.values()):
-        return
+        return ""
 
     console = Console()
     text = Text("Migration Check Failed", style="bold red")
     body = "\n"
-    if any(result.get("DROP_DETECTED")):
+    if any(result.get("DROP_DETECTED") or []):
         body += "🔥 [bold yellow]DROP[/] statements detected (possible data loss)\n"
         for operation in result.get("DROP_DETECTED"):
             line = _find_failing_line(sql, operation)
             if line is not None:
                 body += f"  - Check line {line.get_line_number()}: {line.line_content} \n"
-    if any(result.get("FOREIGN_KEY_WITHOUT_SUPP_INDEX")):
+    if any(result.get("FOREIGN_KEY_WITHOUT_SUPP_INDEX") or []):
         body += "🧩 [bold yellow]FOREIGN KEY[/] without index (slow queries)\n"
         for operation in result.get("FOREIGN_KEY_WITHOUT_SUPP_INDEX"):
             line = _find_failing_line(sql, operation)
             if line is not None:
                 body += f"  - Check line {line.get_line_number()}: {line.line_content} \n"
-    if any(result.get("NON_CONCURRENT_INDEX_BUILDS")):
+    if any(result.get("NON_CONCURRENT_INDEX_BUILDS") or []):
         body += "⏳ [bold yellow]INDEX[/] not built concurrently (table locks)\n"
         for operation in result.get("NON_CONCURRENT_INDEX_BUILDS"):
             line = _find_failing_line(sql, operation)
@@ -34,14 +34,16 @@ def pretty_print(sql, result):
         body += "⚠️  [bold yellow]NOT NULL[/] added without DEFAULT (backfill risk)\n"
 
     console.print(Panel(body, title=text))
+    return body
 
 
 def _find_failing_line(sql, operation) -> Line | None:
     lines_of_sql = sql.split("\n")
     for index, line in enumerate(lines_of_sql):
+        line = line.strip()
         if line is None or line == "":
             continue
         parsed = parse_sql_line_to_ast(line)
         if parsed == operation:
-            return Line(index + 1, line)
+            return Line(index, line)
     return None
---
+++ tests/tests/test_pretty_printer.py
@@ -0,0 +1,33 @@
+from proton_pack.pretty_printer import pretty_print
+from unittest.mock import patch
+
+def test_duplicate_operation_lines():
+    # given
+    with patch("proton_pack.pretty_printer.parse_sql_line_to_ast") as mocked_parse:
+        # given
+        sql = """
+            ALTER TABLE ghost DROP COLUMN name;
+            ALTER TABLE ghost DROP COLUMN type;
+        """
+        result = {
+            "DROP_DETECTED": ["DROP_COL_NAME", "DROP_COL_TYPE"]
+        }
+        # due to _find_failing_line always traversing the whole sql,
+        # parse_sql_line_to_ast is called 3 times in this case
+        mocked_parse.side_effect = [
+            "DROP_COL_NAME", # _find_failing_line(sql, "DROP_COL_NAME") <-- match
+            "DROP_COL_NAME", # _find_failing_line(sql, "DROP_COL_TYPE")
+            "DROP_COL_TYPE" # _find_failing_line(sql, "DROP_COL_TYPE") <-- match
+        ]
+
+        # when
+        print_result = pretty_print(sql, result)
+
+        # then
+        mocked_parse.assert_called()
+        mocked_parse.call_count = 2
+        assert "- Check line 1: ALTER TABLE ghost DROP COLUMN name;" in print_result
+        assert "- Check line 2: ALTER TABLE ghost DROP COLUMN type;" in print_result
+
+
+
