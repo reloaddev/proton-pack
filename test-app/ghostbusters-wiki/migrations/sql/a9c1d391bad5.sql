e6d61cd8c08e11c4a507759aa575aeccd4b9dd51
reloaddev <henrik.pruess@posteo.de>
2026-01-15 18:18:06 +0100

    Fix identified errors

--- test-app/ghostbusters-wiki/migrations/sql/a9c1d391bad5.sql
+++ test-app/ghostbusters-wiki/migrations/sql/a9c1d391bad5.sql
@@ -1,8 +1,10 @@
 -- Running upgrade af3c701704d2 -> a9c1d391bad5
 
-ALTER TABLE ghost ALTER COLUMN reporter_id SET NOT NULL;
+ALTER TABLE ghost ALTER COLUMN reporter_id SET NOT NULL DEFAULT -1;
 
 ALTER TABLE ghost ADD FOREIGN KEY(reporter_id) REFERENCES human (id);
+
+CREATE INDEX CONCURRENTLY ON ghost(reporter_id);
 
 UPDATE alembic_version SET version_num='a9c1d391bad5' WHERE alembic_version.version_num = 'af3c701704d2';
 
