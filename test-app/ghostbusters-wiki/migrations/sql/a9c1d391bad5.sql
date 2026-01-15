f24dc0941cdaed0184b336dd55fa6e0d76618911
reloaddev <henrik.pruess@posteo.de>
2026-01-15 17:53:19 +0100

    Add generated sql migration

---
+++ test-app/ghostbusters-wiki/migrations/sql/a9c1d391bad5.sql
@@ -0,0 +1,10 @@
+-- Running upgrade af3c701704d2 ->a9c1d391bad5
+
+ALTER TABLE ghost ALTER COLUMN reporter_id SET NOT NULL;
+
+ALTER TABLE ghost ADD FOREIGN KEY(reporter_id) REFERENCES human (id);
+
+UPDATE alembic_version SET version_num='a9c1d391bad5' WHERE alembic_version.version_num = 'af3c701704d2';
+
+COMMIT;
+
